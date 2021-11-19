6840381b41965a5ca4e48128d04da9220ff89ed7
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2021-11-19 09:23:40 +0100

    Added communication with the services

--- app/src/main/java/at/ac/htlinn/androidexamples/thingworx/ThingWorxActivity.java
+++ app/src/main/java/at/ac/htlinn/androidexamples/thingworx/ThingWorxActivity.java
@@ -10,8 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,6 +22,7 @@ import java.io.IOException;
 
 import at.ac.htlinn.androidexamples.R;
 import at.ac.htlinn.androidexamples.concurrent.asynctask.SimpleAsyncTaskActivity;
+import lombok.SneakyThrows;
 import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -32,13 +36,14 @@ public class ThingWorxActivity extends AppCompatActivity {
     String appKey = "";
     String baseURl = "";
     String thing = "";
+    String datatable = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         client = new OkHttpClient();
         appKey = getString(R.string.app_key);
         baseURl = getString(R.string.url);
         thing = getString(R.string.thing);
-
+        datatable = getString(R.string.data_table);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thing_worx);
     }
@@ -51,6 +56,17 @@ public class ThingWorxActivity extends AppCompatActivity {
     public void getFromServer(View view)
     {
         GetServerAsyncTask task  = new GetServerAsyncTask();
+        task.execute();
+    }
+
+    public void callService(View view)
+    {
+        CallServiceAsyncTask task = new CallServiceAsyncTask();
+        task.execute();
+    }
+    public void callQuery(View view)
+    {
+        CallQueryAsyncTask task = new CallQueryAsyncTask();
         task.execute();
     }
 
@@ -101,7 +117,6 @@ public class ThingWorxActivity extends AppCompatActivity {
     }
 
     private class GetServerAsyncTask extends AsyncTask<String, Void, String> {
-
         protected String doInBackground(String... urls) {
             String sendKey = ((EditText)findViewById(R.id.key_from_server)).getText().toString();
             String url = String.format("%s/Thingworx/Things/%s/Properties/%s", baseURl, thing, sendKey); //Add the parameter to the url to fetch value
@@ -142,4 +157,191 @@ public class ThingWorxActivity extends AppCompatActivity {
         }
     }
 
+
+    private class CallServiceAsyncTask extends AsyncTask<String, Void, String> {
+        /* This is how the JSON request has to look like:
+        {
+    "values": {
+        "dataShape": {
+            "fieldDefinitions": {
+                "par1": {
+                    "name": "par1",
+                    "baseType": "STRING",
+                },
+                "par2": {
+                    "name": "par2",
+                    "baseType": "NUMBER",
+                }
+            }
+        },
+        "rows": [{
+                "par1": "new10",
+                "par2": 100
+            }
+        ]
+    }
+}
+         */
+        private JSONObject generateDataJSON() throws JSONException
+        {
+            JSONObject fieldDefs = new JSONObject();
+            JSONObject fieldDefsContent = new JSONObject();
+            fieldDefsContent.put("par1", generateFieldDefinition("par1", "STRING"));
+            fieldDefsContent.put("par2", generateFieldDefinition("par2", "NUMBER"));
+            fieldDefs.put("fieldDefinitions", fieldDefsContent);
+
+            JSONObject dataShape = new JSONObject();
+            dataShape.put("dataShape", fieldDefs);
+
+            JSONObject values = new JSONObject();
+            values.put("values", dataShape);
+
+            JSONArray rows = new JSONArray();
+            rows.put(new JSONObject()
+                    .put("par1", "asdf")
+                    .put("par2", 10));
+            dataShape.put("rows", rows);
+            return values;
+        }
+
+        private JSONObject generateFieldDefinition(String name, String baseType) throws JSONException
+        {
+
+            JSONObject def = new JSONObject();
+            def.put("name", name);
+            def.put("baseType", baseType);
+            return def;
+        }
+
+        protected String doInBackground(String... urls) {
+            String sendKey = ((EditText)findViewById(R.id.key_from_server)).getText().toString();
+            String url = String.format("%s/Thingworx/Things/%s/Services/AddOrUpdateDataTableEntry", baseURl, datatable, sendKey); //Add the parameter to the url to fetch value
+            RequestBody body = null;
+            try {
+                String jsonStr = generateDataJSON().toString();
+                Log.d("ThingWorxActivity", jsonStr);
+                body = RequestBody.create(jsonStr, JSON);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Request request = new Request.Builder()
+                    .url(url)
+                    .addHeader("Accept", "application/json")
+                    .addHeader("appkey", appKey)
+                    .post(body)
+                    .build();
+            Log.d("ThingWorxActivity", request.toString());
+            String responseStr = "";
+            try  {
+                Response response = client.newCall(request).execute();
+                //responseStr =  response.body().string();
+                responseStr =  response.body().string();
+                Log.d("ThingWorxActivity", responseStr);
+
+
+                JsonObject obj = new Gson().fromJson(responseStr, JsonObject.class);
+
+                responseStr = obj.getAsJsonArray("rows").get(0).getAsJsonObject().get(sendKey).getAsString();
+            } catch (Exception e)
+            {
+                Log.e("ThingWorxActivity", String.valueOf(e));
+            }
+
+            return responseStr;
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView feedback = findViewById(R.id.service_result);
+            feedback.setText(result);
+        }
+    }
+
+    private class CallQueryAsyncTask extends AsyncTask<String, Void, String> {
+        /* This is how the JSON request has to look like:
+ {"query" :{"filters":
+        {
+            "type": "LT",
+            "fieldName": "par2",
+            "value": "20"
+        }
+      }
+   }
+         */
+        private JSONObject generateDataJSON() throws JSONException
+        {
+
+            JSONObject query = new JSONObject().put("filters", new JSONObject()
+                    .put("type", "LT")
+                    .put("fieldName", "par2")
+                    .put("value", 20));
+
+            return new JSONObject().put("query",query);
+        }
+
+        private JSONObject generateQueryDefinition(String name, String baseType) throws JSONException
+        {
+
+            JSONObject def = new JSONObject();
+            def.put("name", name);
+            def.put("baseType", baseType);
+            return def;
+        }
+
+        protected String doInBackground(String... urls) {
+            String sendKey = ((EditText)findViewById(R.id.key_from_server)).getText().toString();
+            String url = String.format("%s/Thingworx/Things/%s/Services/QueryDataTableEntries", baseURl, datatable, sendKey); //Add the parameter to the url to fetch value
+            RequestBody body = null;
+            try {
+                String jsonStr = generateDataJSON().toString();
+                Log.d("ThingWorxActivity", jsonStr);
+                body = RequestBody.create(jsonStr, JSON);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Request request = new Request.Builder()
+                    .url(url)
+                    .addHeader("Accept", "application/json")
+                    .addHeader("appkey", appKey)
+                    .post(body)
+                    .build();
+            Log.d("ThingWorxActivity", request.toString());
+            String responseStr = "";
+            StringBuilder res = new StringBuilder();
+            try  {
+                Response response = client.newCall(request).execute();
+                responseStr =  response.body().string();
+                Log.d("ThingWorxActivity", responseStr);
+
+
+                JsonObject obj = new Gson().fromJson(responseStr, JsonObject.class);
+
+                JsonArray rows = obj.getAsJsonArray("rows");
+
+                for (JsonElement row : rows)
+                {
+                    JsonObject rowO = row.getAsJsonObject();
+                    res.append(String.format("par1: %s par2: %s\n", rowO.get("par1"), rowO.get("par2")));
+                }
+            } catch (Exception e)
+            {
+                Log.e("ThingWorxActivity", String.valueOf(e));
+            }
+            return res.toString();
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView feedback = findViewById(R.id.service_result);
+            feedback.setText(result);
+        }
+    }
 }
